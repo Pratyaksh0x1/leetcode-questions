286d7e61ffc4b0b4d558da726eaeabf126e17a14
Pratyaksh Tomar <pratyakshtomar2000@gmail.com>
2026-08-21 23:47:10 +0530

    Time: 89 ms (39.54%), Space: 121.7 MB (47.37%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -5,7 +5,7 @@ int knap(vector<int> &cost, vector<int>& time, int i,
     if (n<= 0) {
         return 0;
     }
-     if (i == dp.size()) {
+     if (i == cost.size()) {
             return 1e9;
         }
     if (dp[i][n] != -1) {
