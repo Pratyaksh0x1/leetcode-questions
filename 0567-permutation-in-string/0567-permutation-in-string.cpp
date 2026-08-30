9af8259509226270c2f1d1641470642bf554484f
Pratyaksh Tomar <pratyakshtomar2000@gmail.com>
2026-08-30 11:35:14 +0530

    Time: 3 ms (66.22%), Space: 11 MB (17.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -7,20 +7,16 @@ public:
         }
 
       unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
+      unordered_map<char, int> mp2;
 
-        // Store frequency of s1 and first window of s2
         for (int i = 0; i < k; i++) {
             mp1[s1[i]]++;
             mp2[s2[i]]++;
         }
-
-        // Check first window
         if (mp1 == mp2) {
             return true;
         }
 
-        // Slide the window
         for (int h = k; h < s2.length(); h++) {
          mp2[s2[h - k]]--;
          if (mp2[s2[h - k]] == 0) {
