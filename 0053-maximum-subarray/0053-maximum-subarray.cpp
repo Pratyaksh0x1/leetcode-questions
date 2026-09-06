d8dd4d604d29c4829372c712410996b54f6bed8a
Pratyaksh Tomar <pratyakshtomar2000@gmail.com>
2026-09-06 09:17:54 +0530

    Time: 0 ms (100%), Space: 71.7 MB (81.2%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int be=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int v1=be+nums[i];
+            int v2=nums[i];
+            be=max(v1,v2);
+            ans=max(ans,be);
+        }
+        return ans;
+    }
+};
